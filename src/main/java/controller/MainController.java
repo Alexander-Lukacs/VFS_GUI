45d117fff299e1d21d79a6a3e4f2e2829ecab77e
Alexander <lukacs.alexander@web.de>
2018-01-24 18:40:52 +0100

    First Commit

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private Button btnSettings;
+
+   public void onClick(ActionEvent e) throws RuntimeException, IOException{
+       FXMLLoader lob_loader = new FXMLLoader(getClass().getResource("settings.fxml"));
+       AnchorPane lob_pane = lob_loader.load();
+            Scene lob_scene = new Scene(lob_pane);
+            Stage lob_stage = new Stage();
+            lob_stage.setTitle("Settings");
+            lob_stage.setResizable(false);
+            lob_stage.setScene(lob_scene);
+            lob_stage.show();
+
+    }
+}
