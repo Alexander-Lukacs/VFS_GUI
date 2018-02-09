0df0a3d132722aa101f5e08550154ba3ea54d1e2
FlorianH123 <f.hennrich@gmx.de>
2018-02-09 13:23:01 +0100

    Refactor

--- src/main/java/controller/constants/AlertConstants.java
+++ src/main/java/controller/constants/AlertConstants.java
@@ -10,12 +10,12 @@ public class AlertConstants {
 
     public static final String GC_WARNING_TITLE = "Warning Dialog";
     public static final String GC_WARNING_HEADER = "Warning";
-    public static final String GC_WARNING_IP = "invalid IP";
-    public static final String GC_WARNING_PORT = "invalid Port";
-    public static final String GC_WARNING_PASSWORD = "invalid Password, must have min.8 characters, min.1 capital letter, ,min.1 lower case letter, min.1 number and min.1 special character";
-    public static final String GC_WARNING_EMAIL = "Invalid email address!\n";
-    public static final String GC_WARNING_PASSWORD_NOT_EQUAL = "The passwords are not equal!\n";
-    public static final String GC_WARNING_USERNAME = "Username must have min.3 letters!\n";
+    public static final String GC_WARNING_IP = "Invalid IP!\n\n";
+    public static final String GC_WARNING_PORT = "Invalid Port!\n\n";
+    public static final String GC_WARNING_PASSWORD = "Invalid Password, must have min.8 characters, min.1 capital letter, min.1 lower case letter, min.1 number and min.1 special character!\n\n";
+    public static final String GC_WARNING_EMAIL = "Invalid email address!\n\n";
+    public static final String GC_WARNING_PASSWORD_NOT_EQUAL = "The passwords are not equal!\n\n";
+    public static final String GC_WARNING_USERNAME = "Username must have min.3 letters!\n\n";
 
     public static final String GC_ERROR_TITLE = "Error Dialog";
     public static final String GC_ERROR_HEADER = "Error";
