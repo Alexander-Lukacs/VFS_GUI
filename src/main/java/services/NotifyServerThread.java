537a3db60a36c016a1c7344d5f25737e89a803af
FlorianH123 <f.hennrich@gmx.de>
2018-02-28 22:12:52 +0100

    Refactor

--- src/main/java/services/NotifyServerThread.java
+++ src/main/java/services/NotifyServerThread.java
@@ -11,12 +11,18 @@ public class NotifyServerThread extends Thread {
     private static final int GC_ACCEPT_TIMEOUT_MILLIS = 500;
     private static final int GC_NOTIFY_SERVER_PORT = 32001;
 
+    private static final String GC_MESSAGE_MOVE = "move";
+    private static final String GC_MESSAGE_ADD = "add";
+    private static final String GC_MESSAGE_DELETE = "delete";
+    private static final String GC_MESSAGE_RENAME = "rename";
+
     @Override
     public void run() {
         ServerSocket lob_notifyServer;
         Socket lob_client = null;
         BufferedReader lob_inputStream = null;
         String lva_message;
+        String[] lar_messageArray;
 
         try {
 
@@ -29,12 +35,27 @@ public class NotifyServerThread extends Thread {
                     lob_inputStream = new BufferedReader(new InputStreamReader(lob_client.getInputStream()));
 
                     lva_message = lob_inputStream.readLine();
+                    lar_messageArray = lva_message.split("_");
 
-                    if (lva_message.equals("was ganz tolles")) {
-                        //TODO do amazing stuff
+                    if (lar_messageArray[0].equals(GC_MESSAGE_MOVE)) {
+                        //TODO move file
                     }
 
-                } catch (InterruptedIOException ignored) {}
+                    if (lar_messageArray[0].equals(GC_MESSAGE_ADD)) {
+                        //TODO add new file
+                    }
+
+                    if (lar_messageArray[0].equals(GC_MESSAGE_DELETE)) {
+                        //TODO delete file
+                    }
+
+                    if (lar_messageArray[0].equals(GC_MESSAGE_RENAME)) {
+                        //TODO rename file
+                    }
+
+                } catch (InterruptedIOException ex) {
+                    ex.printStackTrace();
+                }
 
                 if (lob_client != null) {
                     lob_client.close();
