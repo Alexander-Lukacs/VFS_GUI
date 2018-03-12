21eac99c237f3538bf520887a6590531c5f20472
weedric <cedi.schreiner@web.de>
2018-03-12 13:35:24 +0100

    - Todo Liste

---
+++ src/main/java/TODO.java
@@ -0,0 +1,9 @@
+public abstract class TODO {
+    /*
+    TODO Bug: Verschieben von Dateien in oder aus einem Shared Directory
+    TODO FEATURE: Dateien müssen synchronisiert werden wenn sie auf dem pc aktualisiert werden
+    TODO BUG: Registrierungs bug (keine Benachrichtigung für das anlegen eines users)
+    TODO Feature: Shared Directory muss entsprechende Löschfunktion aufrufen wenn es über den Explorer gelöscht wird
+    TODO Feature: Shared DIrectory muss entsprechende Addfunktion aufrufen wenn es über den Explorer hinzugefügt wird
+     */
+}
